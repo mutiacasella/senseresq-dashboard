5c5034848e91d39362383b21a69ba27d257a57c2
Mutia Casella <mutia7762@gmail.com>
2026-09-07 08:48:24 +0700

    Perancangan database dan pemodelan data

---
+++ BackEnd/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./config/database.js");
+
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("SenseResQ Backend is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
